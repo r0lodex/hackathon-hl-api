1c48a0ac31b82f752a8f814e22afdae94417f590
Irfan Radzi <irfan@mrjunior.my>
2019-04-27 19:19:28 +0800

    hardcode way

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/salary', ['middleware'=>'cors','uses'=>'SalaryController@index']);
+Route::get('/salary', 'SalaryController@index');
